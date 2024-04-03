bd4683bf55a73ae67a004ca583715f984302f6d2
REDTJorge <4661368@gmail.com>
2024-04-02 21:21:28 -0600

    Routes funcionales

--- noticiero/src/Componentes/Header.jsx
+++ noticiero/src/Componentes/Header.jsx
@@ -4,10 +4,12 @@ function Header() {
     return (
         <header>
             <div className="logo">
-                <img src="https://i.imgur.com/FguKtbN.png" alt="Logo" />
-                <div className="logo-text">News Environment Project</div>
+            <a href="/"><img src="https://i.imgur.com/FguKtbN.png" alt="Logo" /></a>
+            <a href="/"><div className="logo-text">News Environment Project</div></a>
             </div>
             <nav>
+            
+                <a href="Epic Games">Juegos</a>
                 <a href="Clima">Clima</a>
                 <a href="Noticias">Noticias</a>
                 <a href="Deportes">Deportes</a>
