79df4f9b6597acb29bd2f15178c5f0a2424354c7
REDTJorge <4661368@gmail.com>
2024-04-02 21:10:43 -0600

    CambiosRealizados

--- noticiero/src/App.js
+++ noticiero/src/App.js
@@ -4,6 +4,7 @@ import Clima from './Componentes/Clima';
 import Peliculas from './Componentes/Peliculas';
 import Deportes from './Componentes/Deportes';
 import NoticiasMexico from './Componentes/NoticiasMexico';
+import {Route,Routes,BrowserRouter}from "react-router-dom";
 import Header from './Componentes/Header';
 
 import Carousel from './Componentes/Carrousel'; 
@@ -12,17 +13,42 @@ import Carousel from './Componentes/Carrousel';
 
 function App() {
   return (
-    <div>
-
-      <Header/>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route
+          path="/" element={<MainComponenteWithProps />}
+        />
+        <Route
+          path="/Epic Games" element={<Carousel />}
+        />
+        <Route
+          path="/Clima" element={<Clima />}
+        />
+        <Route
+          path="/Noticias" element={<NoticiasMexico/>}
+        />
+        <Route
+          path="/Deportes" element={<Deportes/>}
+        />
+        <Route
+          path="/Peliculas" element={<Peliculas/>}
+        />
+      </Routes>
       
-      <Carousel />
+    </BrowserRouter>
+  );
+}
 
-      <Clima /> 
-      <Deportes/>
-      <Peliculas/>
-      <NoticiasMexico/>
-    </div>
+function MainComponenteWithProps() {
+  return (
+    <>
+      <Carousel/>
+      <Clima />
+      <Deportes />
+      <Peliculas />
+      <NoticiasMexico />
+    </>
   );
 }
 
